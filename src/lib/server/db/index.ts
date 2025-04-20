cff85f521bb2bbd1cd573faf83d1688a58f76e16
eugenempande <eugene.mpande@gofuseit.com>
2025-04-20 21:03:37 +0200

    another updated

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,11 +1,7 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from './schema';
-import { env } from '$env/dynamic/private';
 
-env.DATABASE_URL = "postgres://root:mysecretpassword@localhost:5432/local";
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-
-const client = postgres(env.DATABASE_URL);
+const client = postgres("postgres://root:mysecretpassword@localhost:5432/local");
 
 export const db = drizzle(client, { schema });
